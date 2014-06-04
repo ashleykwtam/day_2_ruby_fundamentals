711c0a412aae900368fa85ec040c7e932ea51e55
Ashley <ashleykwtam@gmail.com>
2014-06-03 22:45:01 -0400

    playing around with exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#find a good tip for a $55 meal
+
+puts 55/10
+
+#add string and  integer
+
+puts "Goodnight" + 5.to_s
+
+#string interpolation
+a = 45628
+b = 7839
+puts "The value of a is #{a}"
+puts "The value of b is #{b}"
+puts "The value of a times b is #{a*b}"
