948ddb484d79f6cbd436304834f25d45ae3993a6
Ashley <ashleykwtam@gmail.com>
2014-06-03 23:16:08 -0400

    FizzBuzz code

---
+++ exercise4.rb
@@ -0,0 +1,15 @@
+#FizzBuzz code
+
+range = (1..100)
+range.each do |i|
+	if (i%3==0 && i%5==0)
+		puts "Bitmaker"
+	elsif i%3==0 
+		puts "Bit"
+	elsif i%5==0
+		puts "Maker"
+	else 
+		puts i
+	end
+end
+
